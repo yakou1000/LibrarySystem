3c9101dcf16169fa59e31ec0722cb53d44948740
yakou1000 <yakou10004129@gmail.com>
2018-11-20 12:10:09 +0900

    機能修正：Bookクラスにフィールドを追加
    仕様を満たすように変更した

--- Book.java
+++ Book.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
 public class Book{
-
+    int id;             //本ID
+    String name;        //本の名前
+    boolean lent;       //貸し出し状況
+    int returnDate[3];  //返却予定日
+    int registDate[3];  //本の登録日
 }

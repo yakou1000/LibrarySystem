86aff92c5c67c6df6be34eea7ead2f0fd7664b27
yakou1000 <yakou10004129@gmail.com>
2018-11-20 15:27:04 +0900

    機能修正：BookDataBaseにメソッドを追加
    BookDataBaseにbookInformationメソッドを追加
    register,bookInformation,discardメソッドに詳細を追加

--- BookDataBase.java
+++ BookDataBase.java
@@ -6,19 +6,48 @@ public class BookDataBase{
 
     //コンストラクタ
     public BookDataBase(){
-        booklist = new Arraylist<>();
-        bookCounter = 0;
+        bookList = new Arraylist<>();
+        bookCounter = 1;
     }
 
     //本を登録するメソッド
     //本を登録して、bookCounterを1増やす
-    public void register(){
+    public void register(String name){
+        Book newBook = new Book(name,bookCounter);
+        bookCounter += 1;
+        bookList.add(newBook);
+    }
 
+    //名前から本情報を返すメソッド
+    public Book bookInfomation(String name){
+        for(Book b:bookList){
+            if(strcmp(b.getName(),name)==0){
+                return b;
+                break;
+            }
+        }
+        System.out.println("This book don't exist");
+        return null;
+    }
+
+    //idから本情報を返すメソッド
+    public Book bookInformation(int id){
+        for(Book b:bookList){
+            if(b.getId() == id){
+                return b;
+                break;
+            }
+        }
+        System.out.println("This book don't exist");
+        return null;
     }
 
     //本を廃棄するメソッド
-    public void discard(){
-
+    public void discard(int id){
+        Book removeBook = bookList.bookInformation(id);
+        if(removeBook != null){
+            bookList.remove(bookList.indexOf(removeBook));
+        }
     }
 
     //本を貸し出すメソッド
@@ -31,9 +60,10 @@ public class BookDataBase{
 
     }
 
-    //本を検索するメソッド
+    //ユーザーが本を検索するメソッド
     public void search(String name){
-
+        //文字列が一部一致している本の名前・idをセットで出力する
+        //拡張案：貸し出し状況の表示、貸し出し中の場合は返却予定日を表示
     }
 
 }

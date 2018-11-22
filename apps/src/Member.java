1ba5f0df9732704b7b559d9c6872f9732b6a70ac
yakou1000 <yakou10004129@gmail.com>
2018-11-22 15:07:49 +0900

    機能変更：MemberDataBaseの変更
    いくつかのメソッドを追加した。しかし、Memberクラスのメソッドが足りていないためにまだ動作しない。

--- apps/src/Member.java
+++ apps/src/Member.java
@@ -9,8 +9,9 @@ public class Member{
     /*
      * コンストラクタ
      */
-    public Member(String name,boolean ooo){
+    public Member(String name,boolean ooo,int id){
         this.name = name;
+        this.memberId = id;
         this.ooo = ooo;
         this.ownList = new ArrayList<>();
         //ID(登録番号)を生成する
--- apps/src/MemberDataBase.java
+++ apps/src/MemberDataBase.java
@@ -1,17 +1,30 @@
 import java.util.*;
 
 public class MemberDataBase{
-    ArrayList<Member> memberList;
+    private ArrayList<Member> memberList;
+    private int memberCounter;
 
     //コンストラクタ
-    public MemberDataBase(){
+    public MemberDataBase() {
         this.memberList = new ArrayList<>();
+        this.memberCounter = 1;
     }
 
-    /*
-    //会員登録するメソッド
-    public void register(){
 
+    //会員登録するメソッド
+    public void register(String name){
+        Member newMember = new Member(name,false,memberCounter);
+
+        /*
+        //エラーチェック:IDが同じ会員が存在していたら登録しない
+        if(this.bookInformation(bookCounter).isEmpty()) {
+            bookCounter += 1;
+            bookList.add(newBook);
+        }else{
+            System.out.println("This ID is already used");
+            return;
+        }
+        */
     }
 
     //会員を退会するメソッド
@@ -23,7 +36,31 @@ public class MemberDataBase{
     public void change(){
 
     }
-    */
+
+    //todo:以下を変更
+    //名前から本情報を返すメソッド
+    //同じ名前の本をまとめてリストにして返す
+    public ArrayList<Book> bookInformation(String name){
+        ArrayList<Book> ret = new ArrayList<>();
+        for(Book b:bookList){
+            if(b.getName().equals(name)){
+                ret.add(b);
+            }
+        }
+        return ret;
+    }
+
+    //idから会員情報を返すメソッド
+    //扱いを一元化するためにリストで返す
+    public ArrayList<Member> memberInformation(int id){
+        ArrayList<Member> ret = new ArrayList<>();
+        for(Member b:memberList){
+            if(b.getId() == id){
+                ret.add(b);
+            }
+        }
+        return ret;
+    }
 
 }
 

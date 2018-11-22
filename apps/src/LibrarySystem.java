7d0b4255819371baafc19bf2640ae0d17766f9ed
yakou1000 <yakou10004129@gmail.com>
2018-11-22 15:58:51 +0900

    機能追加：LibrarySystemクラスへのメソッド追加
    LibrarySystemクラスに様々なメソッドを追加した。これらはBookDataBase,MemberDataBaseのメソッドである。それらのクラスから呼び出して使用している。

--- apps/src/LibrarySystem.java
+++ apps/src/LibrarySystem.java
@@ -11,23 +11,43 @@ public class LibrarySystem {
     }
 
     //本を登録
+    public void regBook(String name){
+        bookdb.register(name);
+    }
 
     //本を貸し出す
 
     //本を検索
+    public void searchBook(String name){
+        bookdb.search(name);
+    }
 
     //本の一覧を表示
+    public void viewAllBooks(){
+        bookdb.viewAllBooks();
+    }
 
     //本を返却
 
     //本を廃棄する
+    public void discardBook(int id){
+        bookdb.discard(id);
+    }
 
     //会員登録
+    public void regMember(String name){
+        memberdb.register(name);
+    }
 
     //会員退会
+    public void discardMember(int id){
+        memberdb.leave(id);
+    }
 
     //会員情報変更
 
     //会員の一覧表示
-
+    public void viewAllMembers(){
+        memberdb.viewAllMembers();
+    }
 }

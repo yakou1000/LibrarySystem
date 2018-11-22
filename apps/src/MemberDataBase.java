daea22c75332a54a42e3999905b5f9993cae8375
yakou1000 <yakou10004129@gmail.com>
2018-11-22 15:33:10 +0900

    機能追加：MemberDataBaseへのメソッド追加
    MemberDataBaseに会員登録、会員検索、会員退会のメソッドを追加した。

--- apps/src/MemberDataBase.java
+++ apps/src/MemberDataBase.java
@@ -15,36 +15,40 @@ public class MemberDataBase{
     public void register(String name){
         Member newMember = new Member(name,false,memberCounter);
 
-        /*
+
         //エラーチェック:IDが同じ会員が存在していたら登録しない
-        if(this.bookInformation(bookCounter).isEmpty()) {
-            bookCounter += 1;
-            bookList.add(newBook);
+        if(this.memberInformation(memberCounter).isEmpty()) {
+            memberCounter += 1;
+            memberList.add(newMember);
         }else{
             System.out.println("This ID is already used");
             return;
         }
-        */
     }
 
     //会員を退会するメソッド
-    public void leave(){
-
+    public void leave(int id){
+        if(memberInformation(id).isEmpty()){
+            System.out.println("This member is not exist");
+        }else{
+            memberList.remove(memberList.indexOf(this.memberInformation(id).get(0)));
+        }
     }
 
+    /*
     //会員情報を変更するメソッド
     public void change(){
 
     }
+    */
 
-    //todo:以下を変更
-    //名前から本情報を返すメソッド
-    //同じ名前の本をまとめてリストにして返す
-    public ArrayList<Book> bookInformation(String name){
-        ArrayList<Book> ret = new ArrayList<>();
-        for(Book b:bookList){
-            if(b.getName().equals(name)){
-                ret.add(b);
+    //名前から会員情報を返すメソッド
+    //同じ名前の会員をまとめてリストにして返す
+    public ArrayList<Member> memberInformation(String name){
+        ArrayList<Member> ret = new ArrayList<>();
+        for(Member m:memberList){
+            if(m.getName().toLowerCase().contains(name.toLowerCase())){
+                ret.add(m);
             }
         }
         return ret;
@@ -54,9 +58,9 @@ public class MemberDataBase{
     //扱いを一元化するためにリストで返す
     public ArrayList<Member> memberInformation(int id){
         ArrayList<Member> ret = new ArrayList<>();
-        for(Member b:memberList){
-            if(b.getId() == id){
-                ret.add(b);
+        for(Member m:memberList){
+            if(m.getId() == id){
+                ret.add(m);
             }
         }
         return ret;
